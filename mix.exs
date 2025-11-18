7d90c912d8fb7fc862569185fd4f6e65a4534349
Denbeigh Stevens <denbeigh.stevens@discordapp.com>
2025-11-18 19:40:15 +0000

    update pinned version of rustler

--- mix.exs
+++ mix.exs
@@ -24,7 +24,7 @@ defmodule SortedSet.MixProject do
 
   defp deps do
     [
-      {:rustler, "~> 0.31.0"},
+      {:rustler, "~> 0.37.1"},
       {:jemalloc_info, "~> 0.4", runtime: false},
       {:ex_doc, "~> 0.19", only: [:dev], runtime: false},
       {:benchee, "~> 1.0", only: [:dev]},
